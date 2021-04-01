52c9c6f6f3649ad5cb3bb54bbf190534cb963195
Tung Do <dothanhtung132@gmail.com>
2021-04-02 01:48:19 +1030

    add menu items into home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="app">
-      <h1>Tung Test</h1>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './MenuItem.scss';
+
+const MenuItem = ({title, imageUrl, size}) => {
+    return (
+        <div className={`menu-item ${size ? size : ''}`}>
+            <div style={{
+            backgroundImage: `url(${imageUrl})`
+        }} className='background-image'></div>
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='sub-title'>SHOP NOW</span>
+            </div>
+        </div>
+    );
+};
+
+export default MenuItem;
